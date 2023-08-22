e70d0bfb62f3b67b81fc6afdf2d0896f2cd7fc60
chlouzada <chlouzada@gmail.com>
2023-08-22 14:05:32 -0300

    recent store

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,46 @@
 import { useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import './App.css';
+import { useRecentStore } from './stores/recentStore';
+
+export const Recents = () => {
+  const { recents, clearAllRecents } = useRecentStore();
+
+  return (
+    <div className="flex flex-col mx-auto w-4/5 mt-4">
+      <ul className="space-y-2">
+        {recents.map((name) => (
+          <li key={`recents-list-${name}`} className="flex w-full">
+            <span className="w-full border">{name}</span>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={clearAllRecents}>clearAllRecents</button>
+    </div>
+  );
+};
 
 function App() {
   const [name, setName] = useState('');
+  const { addRecent } = useRecentStore();
 
-  async function open_stream(name: string) {
-    console.log('aqui', name);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     await invoke('open_stream', { input: name });
-    console.log('after');
-  }
+    addRecent(name);
+  };
 
   return (
     <div className="flex flex-col justify-center h-full">
       <form
-        className="flex justify-center"
-        onSubmit={(e) => {
-          e.preventDefault();
-          open_stream(name);
-        }}
+        className="flex justify-center gap-2 mx-auto w-4/5"
+        onSubmit={handleSubmit}
       >
-        <input
-          className="mr-2"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a username..."
-        />
+        <input onChange={(e) => setName(e.currentTarget.value)} />
         <button type="submit">Start</button>
       </form>
+      <Recents />
     </div>
   );
 }
---
+++ src/stores/recentStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+
+const RECENTS_KEY = 'recents';
+
+type RecentsState = {
+  recents: string[];
+  addRecent: (recent: string) => void;
+  removeRecent: (recent: string) => void;
+  clearAllRecents: () => void;
+};
+
+export const useRecentStore = create<RecentsState>((set) => ({
+  recents: JSON.parse(localStorage.getItem(RECENTS_KEY) ?? '[]') as string[],
+  addRecent: (recent) =>
+    set((state) => {
+      localStorage.setItem(
+        RECENTS_KEY,
+        JSON.stringify([recent, ...state.recents])
+      );
+      return { recents: [recent, ...state.recents] };
+    }),
+  removeRecent: (recent) =>
+    set((state) => {
+      localStorage.setItem(
+        RECENTS_KEY,
+        JSON.stringify(state.recents.filter((r) => r !== recent))
+      );
+      return { recents: state.recents.filter((r) => r !== recent) };
+    }),
+  clearAllRecents: () => {
+    set({ recents: [] });
+    localStorage.removeItem(RECENTS_KEY);
+  },
+}));
