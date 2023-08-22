6e79aa2c4dbf780565f09d5c231dbf729ecd8630
chlouzada <chlouzada@gmail.com>
2023-08-22 18:05:11 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
 import { useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
-import './App.css';
 import { useRecentStore } from './stores/recentStore';
+import {
+  Container,
+  Button,
+  Avatar,
+  Box,
+  Card,
+  Flex,
+  Text,
+  TextField,
+  ScrollArea,
+} from '@radix-ui/themes';
 
 export const Recents = () => {
   const { recents, clearAllRecents } = useRecentStore();
 
   return (
-    <div className="flex flex-col mx-auto w-4/5 mt-4">
-      <ul className="space-y-2">
+    <ScrollArea type="always" scrollbars="vertical" style={{ height: 200 }}>
+      <Box pr="4">
         {recents.map((name) => (
-          <li key={`recents-list-${name}`} className="flex w-full">
-            <span className="w-full border">{name}</span>
-          </li>
+          <Card className="p-1">
+            <Flex gap="3" align="center">
+              <Avatar
+                size="2"
+                src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+                radius="full"
+                fallback="T"
+              />
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  {name}
+                </Text>
+                <Text as="div" size="1" color="gray">
+                  Playing...
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
         ))}
-      </ul>
-
-      <button onClick={clearAllRecents}>clearAllRecents</button>
-    </div>
+      </Box>
+    </ScrollArea>
   );
 };
 
@@ -27,18 +50,26 @@ function App() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!name) return;
+
     await invoke('open_stream', { input: name });
     addRecent(name);
   };
 
   return (
-    <div className="flex flex-col justify-center h-full">
-      <form
-        className="flex justify-center gap-2 mx-auto w-4/5"
-        onSubmit={handleSubmit}
-      >
-        <input onChange={(e) => setName(e.currentTarget.value)} />
-        <button type="submit">Start</button>
+    <div className="flex flex-col h-full justify-center">
+      <form className="flex justify-center gap-2 mb-6" onSubmit={handleSubmit}>
+        <TextField.Root className="w-full">
+          <TextField.Input
+            size="3"
+            placeholder="Twitch username"
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </TextField.Root>
+        <Button size="3" className="w-1/5" type="submit">
+          Start
+        </Button>
       </form>
       <Recents />
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Theme, Container } from '@radix-ui/themes';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import './styles.css';
+import '@radix-ui/themes/styles.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Theme
+      accentColor="iris"
+      grayColor="gray"
+      panelBackground="solid"
+      radius="large"
+    >
+      <App />
+    </Theme>
+  </React.StrictMode>
 );
