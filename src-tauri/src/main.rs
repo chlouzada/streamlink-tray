dcf3d3e26df6b6099e097b87c5d7e11eb2be60e5
chlouzada <chlouzada@gmail.com>
2023-08-21 22:44:25 -0300

    open streamlink on vlc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+use std::thread;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn open_stream(input: &str) {
+    let input_clone = input.to_string();
+
+    thread::spawn(move || {
+        if cfg!(target_os = "windows") {
+            Command::new("cmd")
+                .args(&["/C", &format!("streamlink twitch.tv/{} best", input_clone)])
+                .output()
+                .expect("failed to execute command")
+        } else {
+            Command::new("sh")
+                .arg("-c")
+                .arg(&format!("streamlink twitch.tv/{} best", input_clone))
+                .output()
+                .expect("failed to execute command")
+        };
+    });
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![open_stream])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
