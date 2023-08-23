a1eeb6675d03d1b9a4095902df9586f52da1d951
chlouzada <chlouzada@gmail.com>
2023-08-23 07:54:46 -0300

    react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Theme } from '@radix-ui/themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 
 import '@radix-ui/themes/styles.css';
 import './styles.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Theme
-      accentColor="iris"
-      grayColor="gray"
-      panelBackground="solid"
-      radius="large"
-    >
-      <App />
-    </Theme>
+    <QueryClientProvider client={queryClient}>
+      <Theme
+        accentColor="iris"
+        grayColor="gray"
+        panelBackground="solid"
+        radius="large"
+      >
+        <App />
+      </Theme>
+    </QueryClientProvider>
   </React.StrictMode>
 );
